4d6c401e52b100033379f128920d99e3a9e96813
Stephan Bouman <stephan@creativecycle.nl>
2026-01-23 17:54:48 +0100

    Use the php executable that is used to run the application

--- src/Support/Composer.php
+++ src/Support/Composer.php
@@ -6,6 +6,9 @@ class Composer
 {
     public function run(string $command): string
     {
-        return shell_exec(sprintf('composer %s', $command));
+        $phpExecutable = PHP_BINARY;
+        $composerExecutable = trim(shell_exec('which composer'));
+
+        return shell_exec(sprintf('%s %s %s', $phpExecutable, $composerExecutable, $command));
     }
 }
